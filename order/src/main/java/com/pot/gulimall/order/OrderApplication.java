b287664689abfabdb24ebbe8ce230a0830b6a52d
PottermoreIron <1327457436@qq.com>
2024-11-23 23:14:08 +0800

    feat: init complete.

---
+++ order/src/main/java/com/pot/gulimall/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.pot.gulimall.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
